12102ecf2dabae39ac6b7d09a66e707c5fd64610
Arjun650 <darjun650@gmail.com>
2024-08-26 02:41:51 +0530

    login signup ui

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#F3B552",
+        secondary: "#F3B552"
+      },
+    },
+  },
+  plugins: [],
+}
+
