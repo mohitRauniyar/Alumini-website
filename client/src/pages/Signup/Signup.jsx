cd85715bc89545c73faf8928551450381b6138d0
Purab-Sen <98190barup@gmail.com>
2024-08-28 19:15:22 +0530

    signupvalidation

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -2,32 +2,36 @@
 import { useState } from "react"
 import PasswordInput from "../../components/input/PasswordInput"
 import { Link, useNavigate } from "react-router-dom"
-import { validateEmail } from "../../utils/helper"
-import toast, { Toaster } from "react-hot-toast"
+import { validateEmail, validNameContent, validatePassword,validateDate,validateForm } from "../../utils/helper"
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Signup = () => {
-    const [name, setName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [reg, setReg] = useState("")
     const [error, setError] = useState(null)
     const [campus, setCampus] = useState("")
+    const [passingYear,setPassingYear]=useState("");
     const notify = () => toast('Here is your toast.');
-
-
     const navigate = useNavigate()
 
     const handleSignUp = async (e) => {
         e.preventDefault();
         if (!reg) {
-            toast.error("Plese enter your Register No")
+            toast.error("Please enter your Register No")
             return;
+        }
+        if(!passingYear){
+            toast.error("Please enter your Passing year")
         }
         if (!campus) {
-            toast.error("Plese enter you campus")
+            toast.error("Please choose campus")
             return;
         }
-        if (!name) {
-            toast.error("Please enter your name");
+        if (!firstName) {
+            toast.error("Please enter your firstName");
             return;
         }
         if (!email) {
@@ -42,27 +46,36 @@ const Signup = () => {
             toast.error("Please enter your password");
             return;
         }
+        if (!validatePassword(password)) {
+            toast.error("Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
+        }
 
-        setError("")
+        let form = {
+            registrationNumber:reg,
+            firstName:firstName,
+            lastName:lastName,
+            campus:campus,
+            email:email,
+            password:password,
+            passingYear:Number.parseInt(passingYear)
+        }
+        const validatedForm = validateForm(form);
+        console.log(validatedForm);
+        try{
+            const URL = ''
+            const response = await fetch()
+        }catch(err){
 
+        }
     }
     return (
+        <>
         <div className="flex items-center justify-center mt-10">
             <div className="py-10 bg-white border rounded w-96 px-7">
                 <form action="" onSubmit={handleSignUp}>
                     <h4 className="text-2xl mb-7">
                         Signup
                     </h4>
-                    {/* <div className="flex gap-3">
-                       <input type="text"  placeholder="Redg. No." className="input-box"
-                            value={reg}
-                            onChange={(e) => setReg(e.target.value)}
-                        />
-                         <input type="text" placeholder="Campus " className="input-box "
-                            value={campus}
-                            onChange={(e) => setCampus(e.target.value)}
-                        />
-                       </div> */}
 
                     <div className="flex gap-2">
                         <input
@@ -70,20 +83,47 @@ const Signup = () => {
                             placeholder="Reg. No."
                             className="flex-1 px-2 input-box"
                             value={reg}
-                            onChange={(e) => setReg(e.target.value)}
+                            onChange={(e) => {
+                                setReg(e.target.value)
+                            }}
                         />
                         <input
                             type="text"
-                            placeholder="Campus"
+                            placeholder="Passing year"
                             className="w-1/3 px-2 input-box"
-                            value={campus}
-                            onChange={(e) => setCampus(e.target.value)}
+                            value={passingYear}
+                            onChange={(e) => {
+                                if(validateDate(e.target.value))
+                                    setPassingYear(e.target.value)
+                            }}
                         />
                     </div>
-
-                    <input type="text" placeholder="Name" className="px-2 input-box"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                    <select className="px-2 input-box" value={campus} placeholder="Campus" 
+                        onChange={(e)=>setCampus(e.target.value)}>
+                            <option value="" disabled>-Select Campus-</option>
+                            <option value="Amritapuri">Amritapuri</option>
+                            <option value="Bengaluru">Bengaluru</option>
+                            <option value="Coimbatore">Coimbatore</option>
+                            <option value="Kochi">Kochi</option>
+                            <option value="Chennai">Chennai</option>
+                            <option value="Amravati">Amravati</option>
+                            <option value="Mysuru">Mysuru</option>
+                    </select>
+                    <input type="text" placeholder="Firstname" className="px-2 input-box"
+                        value={firstName}
+                        onChange={(e) => {
+                            if (validNameContent(e.target.value))
+                                setFirstName(e.target.value)
+                        }
+                        }
+                    />
+                    <input type="text" placeholder="Lastname" className="px-2 input-box"
+                        value={lastName}
+                        onChange={(e) => {
+                            if (validNameContent(e.target.value))
+                                setLastName(e.target.value)
+                        }
+                        }
                     />
 
                     <input type="text" placeholder="Email" className="px-2 input-box"
@@ -106,10 +146,9 @@ const Signup = () => {
                     </p>
                 </form>
             </div>
-            <Toaster position="top-right"
-            />
-
         </div>
+        <ToastContainer theme="colored"/>
+        </>
     )
 }
 
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,19 +1,42 @@
-export const validateEmail = (email) =>{
+export const validateEmail = (inputEmail) =>{
+    const email = inputEmail.trim()
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email)
 }
+export const validNameContent = (nameContent)=>{
+    if (/^[A-Za-z ]+$/.test(nameContent) || nameContent == "") return true;
+    else return false;
+}
+export const validatePassword = (password)=>{
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+}
+export const validateDate = (date) => {
+    if(date == "")return true;
+    if(/^\d+$/.test(date)){
+        const val = Number.parseInt(date);
+        const currentyear = Number.parseInt(new Date().getFullYear());
+        if(val<=currentyear)return true;
+        else return false;
+    }
+    return false;
+}
+export const validateForm = (form)=>{
+    const firstName = getName(form.firstName);
+    const lastName = getName(form.lastName);
+    const registrationNumber = form.registrationNumber.trim()
+    const email = form.email.trim();
+    return {...form,firstName:firstName,lastName:lastName,registrationNumber:registrationNumber,email:email};
+}
 
-// export const getInitials = (name) =>{
-
-//     // name = name.trim(); 
-//     if(!name) return " "; 
-
-//     const words = name.split(" ");
-//     let initials = ""; 
-
-//     for(let i = 0 ; i< Math.min(words.length, 2); i++ ){
-//         initials += words[i][0];
-//         }
-
-//         return initials.toUpperCase();
-// }
+const getName = (name)=>{
+    let array = name.trim().split(" ");
+    let newName = "";
+    array.forEach((val)=>{
+        if(/^[A-Za-z]+$/.test(val)){
+            let capitalizedWord = val.charAt(0).toUpperCase() + val.slice(1).toLowerCase();
+            newName+=capitalizedWord+" ";
+        }
+    })
+    newName = newName.trim();
+    return newName;
+}
