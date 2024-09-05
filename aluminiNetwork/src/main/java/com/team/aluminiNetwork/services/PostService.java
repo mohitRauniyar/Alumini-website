ec92a715e2ab863b1abded7243d4a4393856b21b
ojas tyagi <ojastyagi753@gmail.com>
2024-09-05 21:52:46 +0530

    add create post api route

--- aluminiNetwork/src/main/java/com/team/aluminiNetwork/services/PostService.java
+++ aluminiNetwork/src/main/java/com/team/aluminiNetwork/services/PostService.java
@@ -2,6 +2,7 @@ package com.team.aluminiNetwork.services;
 
 import com.team.aluminiNetwork.models.Post;
 import com.team.aluminiNetwork.repositories.PostRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
     private final PostRepository postRepository;
-    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public PostService(PostRepository postRepository, MongoTemplate mongoTemplate) {
+    public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
-        this.mongoTemplate = mongoTemplate;
     }
 
     public void createPost(Post post) {
         postRepository.save(post);
+    }
+
+    public Post getPostById(ObjectId id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public void updatePost(Post existingPost) {
+        postRepository.save(existingPost);
     }
 }
